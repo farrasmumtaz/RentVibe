367ac3b4fce6348d5f798ec268b42a2fb966646d
farrasmumtaz <farrasmumtaz10@gmail.com>
2026-07-16 13:42:52 +0700

    feat(category): implement get category endpoints

--- internal/category/service.go
+++ internal/category/service.go
@@ -4,6 +4,8 @@ import "github.com/farrasmumtaz/RentVibe/internal/models"
 
 type Service interface {
 	Create(category *models.Category) error
+	FindAll(search string, page int, limit int) ([]models.Category, int64, error)
+	FindByID(id uint) (*models.Category, error)
 }
 
 type service struct {
@@ -18,4 +20,12 @@ func NewService(repository Repository) Service {
 
 func (s *service) Create(category *models.Category) error {
 	return s.repository.Create(category)
+}
+
+func (s *service) FindAll(search string, page int, limit int) ([]models.Category, int64, error) {
+	return s.repository.FindAll(search, page, limit)
+}
+
+func (s *service) FindByID(id uint) (*models.Category, error) {
+	return s.repository.FindByID(id)
 }
